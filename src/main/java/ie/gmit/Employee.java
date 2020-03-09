1c38a749371bafe4fa773c24929d3cea38534a0c
CalebFox96 <g00327455@gmit.ie>
2020-03-09 10:04:27 +0000

    Creation of Constructor & getters

--- src/main/java/ie/gmit/Employee.java
+++ src/main/java/ie/gmit/Employee.java
@@ -5,6 +5,45 @@ public class Employee {
     private String name;
     private String PPS;
     private String number;
+    private String employmentType;
+    private int age;
+
+    public Employee(String title, String name, String PPS, String number, String employmentType, int age) {
+        if(title.equals("mr")||title.equals("mrs")||title.equals("ms")){
+            this.title = title;
+        }
+        else
+            throw new IllegalArgumentException("Invalid Title");
+        if(name.length()>3 && name.length()< 25){
+            this.name = name;
+        }
+        else
+            throw new IllegalArgumentException("Invalid name");
+        if(PPS.length()>8&&PPS.length()<8){
+            this.PPS = PPS;
+        }
+        else
+            throw new IllegalArgumentException("Invalid PPS");
+        if(number.length()>9 && number.length()<9){
+            this.number =number;
+        }
+        else
+            throw new IllegalArgumentException("Invalid number");
+
+        if(employmentType.equals("full")||employmentType.equals("part")||employmentType.equals("contract")){
+            this.employmentType = title;
+        }
+        else
+            throw new IllegalArgumentException("Invalid number");
+
+        if(age >18 ){
+            this.name = name;
+        }
+        else
+            throw new IllegalArgumentException("Invalid age, too young to work at company");
+
+    }
+
 
     public String getTitle() {
         return title;
@@ -20,16 +59,5 @@ public class Employee {
 
     public String getNumber() {
         return number;
-    }
-    private String employmentType;
-    private int age;
-
-    public Employee(String title, String name, String PPS, String number, String employmentType, int age) {
-        if(title.equals("mr")||title.equals("mrs")||title.equals("ms")){
-            this.title = title;
-        }
-        if(name.length()>3 && name.length()< 25){
-            this.name = name;
-        }
     }
 }
