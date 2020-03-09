f00b223930e7d0961157435e1e2d5404e8aac5a7
CalebFox96 <g00327455@gmit.ie>
2020-03-09 10:05:02 +0000

    Creation of test functions

--- src/test/java/ie/gmit/EmployeeTest.java
+++ src/test/java/ie/gmit/EmployeeTest.java
@@ -14,7 +14,31 @@ public class EmployeeTest {
     @Test
     void testConstructor(){
         Employee myEmployee = new Employee("mr","caleb","12345678","123456789","full",23);
-        assertEquals("caleb",myEmployee.getTitle());
+        assertEquals("mr",myEmployee.getTitle());
+
+    }
+    @Test
+    void testNameConstructor(){
+        Employee myEmployee = new Employee("mr","caleb","12345678","123456789","full",23);
+        assertEquals("mr",myEmployee.getName());
+
+    }
+    @Test
+    void testPPSConstructor(){
+        Employee myEmployee = new Employee("mr","caleb","12345678","123456789","full",23);
+        assertEquals("mr",myEmployee.getPPS());
+
+    }
+    @Test
+    void testNumberConstructor(){
+        Employee myEmployee = new Employee("mr","caleb","12345678","123456789","full",23);
+        assertEquals("mr",myEmployee.getTitle());
+
+    }
+    @Test
+    void testNumberConstructor(){
+        Employee myEmployee = new Employee("mr","caleb","12345678","123456789","full",23);
+        assertEquals(23,myEmployee.getTitle());
 
     }
 
